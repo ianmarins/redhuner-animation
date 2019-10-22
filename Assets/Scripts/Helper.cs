0b8e90580257f9ca8741593f706410934a1960ad
Eduardo Henrique Camero <ehcamero@gmail.com>
2019-10-22 10:37:47 -0300

    Teste

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Helper
+{
+    public static GameObject createCube(float x, float y, float z)
+    {
+        // Criando um objeto do tipo cubo
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        // Criando um vetor de posição para ser atribuiido ao cubo posteriormente
+        Vector3 posicao = new Vector3(x, y, z);
+        Vector3 escala = new Vector3(x, y, z);
+        // Atribuindo uma posição ao cubo criado
+        cube.transform.position = posicao;
+        cube.transform.localScale = escala;
+        
+
+        return cube;
+    }
+}
